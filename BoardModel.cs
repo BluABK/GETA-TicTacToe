87046eaa0be0b05deabbf41803edcb0d2b3e1185
BluABK <bluabk@bluabk.net>
2021-08-20 13:51:26 +0200

    Add project files.

---
+++ BoardModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToe
+{
+    class BoardModel
+    {
+        private const int BlankId = 0;
+        private const int CrossId = 1;
+        private const int CircleId = 2;
+        private int[] board = new int[9];
+
+        public BoardModel()
+        {
+
+        }
+    }
+}
